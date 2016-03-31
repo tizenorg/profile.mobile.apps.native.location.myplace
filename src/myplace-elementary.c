eec7a123afbc2897864f669e0812683e3b439257
jongmun.woo <jongmun.woo@samsung.com>
2016-03-31 11:40:49 +0900

    modify to use map-service 3.0 API
    Signed-off-by: jongmun.woo <jongmun.woo@samsung.com>
    Change-Id: I250df046d8f69d9878a4f6cd4d8ae473fbecebae

--- src/myplace-elementary.c
+++ src/myplace-elementary.c
@@ -29,6 +29,11 @@
 
 #include "myplace-common.h"
 
+static void popup_timeout_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	evas_object_del(obj);
+}
+
 Evas_Object *create_popup(Evas_Object *parent, char *style, char *text)
 {
 	Evas_Object *popup;
@@ -60,6 +65,7 @@ void toast_popup(myplace_app_data *ad, char *str)
 	popup = create_popup(ad->nf, "toast", str);
 	evas_object_size_hint_weight_set(popup, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 	elm_popup_timeout_set(popup, 2.0);
+	evas_object_smart_callback_add(popup, "timeout", popup_timeout_cb, NULL);
 
 	evas_object_show(popup);
 }
